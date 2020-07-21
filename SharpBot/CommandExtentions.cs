198e918ea108396fa1685390ffb44fd48f6fea35
Cameron <cameronproulx@outlook.com>
2020-07-21 14:14:41 -0400

    removed unnecesary usings

--- SharpBot/CommandExtentions.cs
+++ SharpBot/CommandExtentions.cs
@@ -1,9 +1,7 @@
 ﻿using Discord.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 public class CommandExtentions
--- SharpBot/Modules/GuildModule.cs
+++ SharpBot/Modules/GuildModule.cs
@@ -2,10 +2,6 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 public class GuildModule : ModuleBase<SocketCommandContext>
