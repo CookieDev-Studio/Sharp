bb2e940ce05f29c8020910cb2721e3f083436c15
Cameron <cameronproulx@outlook.com>
2020-07-23 15:14:05 -0400

    moved sqlConection string to a perminent file

--- Sharp.Data/DataExtentions.cs
+++ Sharp.Data/DataExtentions.cs
@@ -8,15 +8,7 @@ namespace SharpBot.Data
     {
         public static NpgsqlConnection GetConnection()
         {
-            string connectionString;
-
-            try { connectionString = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "SqlConnectionString.txt")); }
-            catch
-            {
-                Console.WriteLine("Enter sql Conection String:");
-                connectionString = Console.ReadLine();
-                File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "SqlConnectionString.txt"), connectionString);
-            }
+            string connectionString = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "SqlConnectionString.txt"));
             return new NpgsqlConnection(connectionString);
         }
     }
