fd3ecb5caa2174bfb50bfb94683cbc95e4b6679a
Cameron <cameronproulx@outlook.com>
2020-07-25 22:09:20 -0400

    added unit tests for GuildService

--- Sharp.Service.Tests/GuildServiceTests.cs
+++ Sharp.Service.Tests/GuildServiceTests.cs
@@ -3,21 +3,167 @@ using Moq.AutoMock;
 using Sharp.Data;
 using Sharp.Service;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 public class GuildServiceTests
 {
+    private readonly Sharp.Data.Config testConfig = new Sharp.Data.Config()
+    {
+        mod_Channel_Id = "123456789123456789",
+        prefix = '-',
+        message_log = false
+    };
+
+    [Fact]
+    public void GetModChannel()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfig(0)).Returns(testConfig);
+
+        var guildService = new GuildService(mock.Object);
+
+        ulong actual = guildService.GetModChannel(0);
+        ulong expected = 123456789123456789;
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void GetModChannelAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfigAsync(0)).Returns(Task.FromResult(testConfig));
+
+        var guildService = new GuildService(mock.Object);
+
+        var actual = guildService.GetModChannelAsync(0);
+        var expected = Task.FromResult<ulong>(123456789123456789);
+
+        Assert.Equal(expected.Result, actual.Result);
+    }
+
+    [Fact]
+    public void SetModChannel()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetModChannel(0,0);
+
+        mock.Verify(x => x.SetModChannel(0, 0), Times.Exactly(1));
+    }
+    [Fact]
+    public void SetModChannelAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetModChannelAsync(0, 0);
+
+        mock.Verify(x => x.SetModChannelAsync(0, 0), Times.Exactly(1));
+    }
+
+    [Fact]
+    public void GetPrefix()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfig(0)).Returns(testConfig);
+
+        var guildService = new GuildService(mock.Object);
+
+        char actual = guildService.GetPrefix(0);
+        char expected = '-';
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void GetPrefixAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfigAsync(0)).Returns(Task.FromResult(testConfig));
+
+        var guildService = new GuildService(mock.Object);
+
+        var actual = guildService.GetPrefixAsync(0);
+        var expected = Task.FromResult('-');
+
+        Assert.Equal(expected.Result, actual.Result);
+    }
+
+    [Fact]
+    public void SetPrefix()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetPrefix(0, '-');
+
+        mock.Verify(x => x.SetPrefix(0, '-'), Times.Exactly(1));
+    }
+    [Fact]
+    public void SetPrefixAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetPrefixAsync(0, '-');
+
+        mock.Verify(x => x.SetPrefixAsync(0, '-'), Times.Exactly(1));
+    }
+
+    [Fact]
+    public void GetMessageLog()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfig(0)).Returns(testConfig);
+
+        var guildService = new GuildService(mock.Object);
+
+        bool actual = guildService.GetMessageLog(0);
+        bool expected = false;
+
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void GetMessageLogAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfigAsync(0)).Returns(Task.FromResult(testConfig));
+
+        var guildService = new GuildService(mock.Object);
+
+        var actual = guildService.GetMessageLogAsync(0);
+        var expected = Task.FromResult(false);
+
+        Assert.Equal(expected.Result, actual.Result);
+    }
+
+    [Fact]
+    public void SetMessageLog()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetMessageLog(0, false);
+
+        mock.Verify(x => x.SetMessageLog(0, false), Times.Exactly(1));
+    }
+    [Fact]
+    public void SetMessageLogAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.SetMessageLogAsync(0, false);
+
+        mock.Verify(x => x.SetMessageLogAsync(0, false), Times.Exactly(1));
+    }
+
     [Fact]
     public void GetConfig()
     {
         var mock = new Mock<IGuildData>();
-        mock.Setup(x => x.GetGuildConfig(0)).Returns(
-            new Sharp.Data.Config()
-            {
-                mod_Channel_Id = "123456789123456789",
-                prefix = '-',
-                message_log = false
-            });
+        mock.Setup(x => x.GetGuildConfig(0)).Returns(testConfig);
 
         var guildService = new GuildService(mock.Object);
 
@@ -30,5 +176,50 @@ public class GuildServiceTests
         };
 
         Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void GetConfigAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        mock.Setup(x => x.GetGuildConfigAsync(0)).Returns(
+            Task.FromResult(new Sharp.Data.Config()
+            {
+                mod_Channel_Id = "123456789123456789",
+                prefix = '-',
+                message_log = false
+            }));
+
+        var guildService = new GuildService(mock.Object);
+
+        var actual = guildService.GetConfigAsync(0);
+        var expected = Task.FromResult(new Sharp.Service.Config
+        {
+            ModChannelId = 123456789123456789,
+            Prefix = '-',
+            MessageLog = false
+        });
+
+        Assert.Equal(expected.Result, actual.Result);
+    }
+
+    [Fact]
+    public void AddConfig()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.AddConfig(0, 0);
+
+        mock.Verify(x => x.AddConfig(0, 0, '-', false), Times.Exactly(1));
+    }
+    [Fact]
+    public void AddConfigAsync()
+    {
+        var mock = new Mock<IGuildData>();
+        var guildService = new GuildService(mock.Object);
+
+        guildService.AddConfigAsync(0, 0);
+
+        mock.Verify(x => x.AddConfigAsync(0, 0, '-', false), Times.Exactly(1));
     }
 }
---
+++ Sharp.Service/IGuildService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Sharp.Service
+{
+    public interface IGuildService
+    {
+        void AddConfig(ulong guildId, ulong ModchannelId, char prefix = '-', bool messagelog = false);
+        Task AddConfigAsync(ulong guildId, ulong ModchannelId, char prefix = '-', bool messagelog = false);
+        Config GetConfig(ulong guildId);
+        Task<Config> GetConfigAsync(ulong guildId);
+        bool GetMessageLog(ulong guildId);
+        Task<bool> GetMessageLogAsync(ulong guildId);
+        ulong GetModChannel(ulong guildId);
+        Task<ulong> GetModChannelAsync(ulong guildId);
+        char GetPrefix(ulong guildId);
+        Task<char> GetPrefixAsync(ulong guildId);
+        void SetMessageLog(ulong guildId, bool value);
+        Task SetMessageLogAsync(ulong guildId, bool value);
+        void SetModChannel(ulong guildId, ulong channelId);
+        Task SetModChannelAsync(ulong guildId, ulong channelId);
+        void SetPrefix(ulong guildId, char prefix);
+        Task SetPrefixAsync(ulong guildId, char prefix);
+    }
+}
