1d6687d98dc040c56509782d116ee5da5077a301
Cameron <cameronproulx@outlook.com>
2020-07-26 01:11:56 -0400

    readded Sharp.Data.Tests

--- Sharp.Data/DataExtentions.cs
+++ Sharp.Data/DataExtentions.cs
@@ -17,7 +17,8 @@ namespace Sharp.Data
                 $"Username={dbConnection.Username};" +
                 $"Password={dbConnection.Password};" +
                 $"Database={dbConnection.Database};" +
-                $"sslmode=Require;Trust Server Certificate=true");
+                $"sslmode=Require;" +
+                $"Trust Server Certificate=true");
         }
     }
 }
