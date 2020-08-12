5fe587c00cc9f5c63f1a55f09dc0a805b3eb69ed
Cameron <cameronproulx@outlook.com>
2020-08-12 16:34:48 -0400

    added ability to set a role to a user depending on what link they used

---
+++ Sharp.Data/Models/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp.Data
+{
+    public class Link
+    {
+        public string guild_id;
+        public string code;
+        public string role_id;
+        public int uses;
+    }
+}
---
+++ Sharp/Modules/LinkModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Sharp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LinkModule : ModuleBase
+{
+    readonly LinkService _linkService;
+    public LinkModule(LinkService linkService) => _linkService = linkService;
+
+    [Command("pair")]
+    [Summary("adds a user to the role when they join from the link")]
+    [RequireUserPermission(Discord.ChannelPermission.ManageChannels)]
+    public async Task AddLinkRolePair(string linkCode, SocketRole role)
+    {
+        
+        if (linkCode.StartsWith("https://discord.gg/"))
+            linkCode = linkCode.Substring(19);
+
+        var invites = await Context.Guild.GetInvitesAsync();
+        //if link exists
+        if (Context.Guild.GetInvitesAsync().Result.Any(x => x.Code == linkCode))
+        {
+            
+
+            await _linkService.AddLinkRolePairAsync(Context.Guild.Id, linkCode, role.Id, (int)invites.First(x => x.Code == linkCode).Uses);
+            await ReplyAsync($"users will now join {role.Mention} when joining from link");
+        }
+        else
+            await ReplyAsync($"link does not exist");
+    }
+}
+
