e08fda3ee69d2a53300df0bc72aca63c2e841db7
Cameron <cameronproulx@outlook.com>
2020-08-22 21:32:56 -0400

    removed unused using

--- Sharp.Api/Controllers/UserController.cs
+++ Sharp.Api/Controllers/UserController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Sharp.Service;
