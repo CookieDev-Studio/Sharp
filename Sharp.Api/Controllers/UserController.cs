257fbea7c32e4fefeefffb1f51ef0e4b9fcf6512
Cameron <cameronproulx@outlook.com>
2020-08-22 20:13:31 -0400

    feature is now functionnal

--- Sharp.Api/Controllers/UserController.cs
+++ Sharp.Api/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Sharp.Service;
 
 namespace Sharp.Api.Controllers
@@ -17,25 +18,21 @@ namespace Sharp.Api.Controllers
         readonly GuildService _guildService;
         public UserController(GuildService guildService) => _guildService = guildService;
 
-        [HttpGet("commonguilds/{token}")]
+        [HttpGet("guildswithsharp/{token}")]
         public async Task<ActionResult> GetCommonGuildsAsync(string token)
         {
-            //list of all guilds user is in
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
 
             var response = await client.GetAsync("https://discordapp.com/api/users/@me/guilds");
+            var userGuilds = JsonConvert.DeserializeObject<PartialGuild[]>(await response.Content.ReadAsStringAsync());
 
-            var userGuilds = await response.Content.ReadAsStringAsync();
-            //EXTRACT GUILD ID
-            var userGuildIds = new ulong[0];
-
-            //list of all guilds sharp is in 
             var guildIds = await _guildService.GetAllGuildsAsync();
 
-            //return list of common guilds
-            return Ok(userGuildIds.Where(x => guildIds.Contains(x)));
+            foreach (var guild in userGuilds)
+                guild.Icon = $"https://cdn.discordapp.com/icons/{guild.Id}/{guild.Icon}";
+            return Ok(JsonConvert.SerializeObject(userGuilds.Where(x => guildIds.Contains(x.Id))));
         }
     }
 }
---
+++ Sharp.Api/ResponseModels/PartialGuild.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class PartialGuild
+{
+    [JsonProperty(PropertyName = "id")]
+    public ulong Id { get; set; }
+    [JsonProperty(PropertyName = "name")]
+    public string Name { get; set; }
+    [JsonProperty(PropertyName = "icon")]
+    public string? Icon { get; set; }
+    [JsonProperty(PropertyName = "owner")]
+    public bool Owner { get; set; }
+    [JsonProperty(PropertyName = "permissions")]
+    public int Permissions { get; set; }
+    [JsonProperty(PropertyName = "permissions_new")]
+    public string PermissionsNew { get; set; }
+}
